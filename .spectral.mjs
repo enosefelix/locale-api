30c39d5613d9b6426c4daa2d1adc4e97919e44f5
Abhademere Enosejolagbon <enosefelix@gmail.com>
2023-05-19 07:21:27 +0100

    Modified 6 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwMTUxNA/spectral.js?branch_slug=main&token=22e3b0a0-9fdf-46db-980c-f0627300f4fc";
+export default { extends: ruleset };
